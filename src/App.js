49294339654e234258eb22bbfd4c278bd4fcd83b
Forrest Guilloud <fbguillo@gmail.com>
2017-07-02 13:47:44 -0400

    Fixed header.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Under Constraeeection..</h2>
+        <h2>Under Construction...</h2>
       </div>
     );
   }
