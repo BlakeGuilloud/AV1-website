64f60ab05e78cc5fa07c622fc99af574ff66d37e
Blake Guilloud <blake.guilloud@gmail.com>
2017-07-30 13:08:11 -0400

    Loader animation :fire:

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="img__wrapper" />
         <img src={logo} alt="i have failed you" />
       </div>
     );
