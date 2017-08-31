51f5c029120c7e0ea9debcdbc3c70ac486ca52bd
Blake Guilloud <blake.guilloud@gmail.com>
2017-08-30 20:37:27 -0400

    some styling.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,34 +2,10 @@ import React, { Component } from 'react';
 
 class Blog extends Component {
   render() {
-    const renderBlogItem = () => (
-      <div className="blog__item">
-        <img className="blog__item-image" src="http://www.fillmurray.com/200/300" />
-        <div className="blog__item-content">
-          <div className="blog__item-main">
-            <div className="blog__item-title">
-              Writing Serverless Apps with NodeJS
-            </div>
-            <div className="blog__item-summary">
-              My journey in and around serverless architecture.
-            </div>
-          </div>
-          <div className="blog__item-footer">
-            <div className="blog__item-author">
-              Blake Guilloud
-            </div>
-            <div className="blog__item-timestamp">
-              8 / 27 / 17
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-
     return (
       <div className="blog">
         <div className="blog__item">
-          <img className="blog__item-image" src="http://www.fillmurray.com/200/300" />
+          <div className="blog__item-image image__bill" />
           <div className="blog__item-content">
             <div className="blog__item-main">
               <div className="blog__item-title">
@@ -50,7 +26,112 @@ class Blog extends Component {
           </div>
         </div>
         <div className="blog__item">
-          <img className="blog__item-image" src="http://www.fillmurray.com/500/300" />
+          <div className="blog__item-image image__bill2" />
+          <div className="blog__item-content">
+            <div className="blog__item-main">
+              <div className="blog__item-title">
+                Writing Serverless Apps with NodeJS
+              </div>
+              <div className="blog__item-summary">
+                My journey in and around serverless architecture.
+              </div>
+            </div>
+            <div className="blog__item-footer">
+              <div className="blog__item-author">
+                Blake Guilloud
+              </div>
+              <div className="blog__item-timestamp">
+                8 / 27 / 17
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog__item">
+          <div className="blog__item-image image__bill2" />
+          <div className="blog__item-content">
+            <div className="blog__item-main">
+              <div className="blog__item-title">
+                Writing Serverless Apps with NodeJS
+              </div>
+              <div className="blog__item-summary">
+                My journey in and around serverless architecture.
+              </div>
+            </div>
+            <div className="blog__item-footer">
+              <div className="blog__item-author">
+                Blake Guilloud
+              </div>
+              <div className="blog__item-timestamp">
+                8 / 27 / 17
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog__item">
+          <div className="blog__item-image image__bill2" />
+          <div className="blog__item-content">
+            <div className="blog__item-main">
+              <div className="blog__item-title">
+                Writing Serverless Apps with NodeJS
+              </div>
+              <div className="blog__item-summary">
+                My journey in and around serverless architecture.
+              </div>
+            </div>
+            <div className="blog__item-footer">
+              <div className="blog__item-author">
+                Blake Guilloud
+              </div>
+              <div className="blog__item-timestamp">
+                8 / 27 / 17
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog__item">
+          <div className="blog__item-image image__bill2" />
+          <div className="blog__item-content">
+            <div className="blog__item-main">
+              <div className="blog__item-title">
+                Writing Serverless Apps with NodeJS
+              </div>
+              <div className="blog__item-summary">
+                My journey in and around serverless architecture.
+              </div>
+            </div>
+            <div className="blog__item-footer">
+              <div className="blog__item-author">
+                Blake Guilloud
+              </div>
+              <div className="blog__item-timestamp">
+                8 / 27 / 17
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog__item">
+          <div className="blog__item-image image__bill2" />
+          <div className="blog__item-content">
+            <div className="blog__item-main">
+              <div className="blog__item-title">
+                Writing Serverless Apps with NodeJS
+              </div>
+              <div className="blog__item-summary">
+                My journey in and around serverless architecture.
+              </div>
+            </div>
+            <div className="blog__item-footer">
+              <div className="blog__item-author">
+                Blake Guilloud
+              </div>
+              <div className="blog__item-timestamp">
+                8 / 27 / 17
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog__item">
+          <div className="blog__item-image image__bill2" />
           <div className="blog__item-content">
             <div className="blog__item-main">
               <div className="blog__item-title">
